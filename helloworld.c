ade8c46ea40336852e3ce09a969beec4b3bf512c
Shruti <shrut.g@gmail.com>
2017-01-26 16:24:53 -0500

    Initial test file to test git commands

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+/* Helloworld test file 
+ * Filename: helloworld.c
+ * Created to test git commands
+ * Author: Shruti
+ * Date: 01/26/2017
+*/
+
+#include<stdio.h>
+
+void main(int argc, char *argv[]){
+	printf("Hello world!\n");
+}
